ed4580a7bf5977815d4b489fe376295a1b1070c7
andreanerla <54369118+andreanerla@users.noreply.github.com>
2020-04-19 14:34:45 +0200

    Update etl_us_food.py

--- FDA_regulated_products/etl_us_food.py
+++ FDA_regulated_products/etl_us_food.py
@@ -1,15 +1,14 @@
+
 import requests
 import pandas as pd
 import json
-import pyodbc
 import os
 from datetime import datetime
 
-r = requests.get('https://api.fda.gov/food/enforcement.json?limit=100') #pulling the first 100 rows from the website endpoint (unfortunately I'm not allowed to pull more than that) 
-
+r = requests.get('https://api.fda.gov/food/enforcement.json?limit=100') #pulling the first 100 rows from the website endpoint (unfortunately I'm not allowed to pull mo$
 data = open('enforcement.json', 'wb').write(r.content) # saving the json file locally
 
-data = json.load(open("enforcement.json")) #opening the json file 
+data = json.load(open("enforcement.json")) #opening the json file
 
 df = pd.DataFrame(data["results"]) #saving the json file as pandas dataframe
 
@@ -19,9 +18,5 @@ csv_df = df.to_csv("fda_csv.csv") #converting the file as CSV
 
 now = datetime.now()
 
-now_date = str(now.year) + "-" + str(now.month) + "-" + str(now.day) + "-" + str(now.hour) + "-" + str(now.minute) #assigning the current date to the file which will be uploaded on S3
-
-os.system(r'aws s3 cp C:\Users\andre\Documents\fda_csv.csv s3://fda-regulated-products/fda_' + now_date + '.csv') #uploading the file to S3 bucket with amazon command line interface
-
-
-
+now_date = str(now.year) + "-" + str(now.month) + "-" + str(now.day) + "-" + str(now.hour) + "-" + str(now.minute) #assigning the current date to the file which will b$
+os.system(r'aws s3 cp ./python_linux_files/fda_csv.csv s3://fda-regulated-products/fda_' + now_date + '.csv') #uploading the file to S3 bucket with amazon command line$
